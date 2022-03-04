732ebb2dd8eb303138b868df73f12a08f5b159a4
Thiago <thiagolopespereiralp1@gmail.com>
2022-03-04 18:11:19 -0300

    terminei a licao

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("FOGO!!");
+Console.ResetColor();
